6c18dd0e525116fe3a09d7e595e5473d66bbb549
Crowdbotics <team@crowdbotics.com>
2021-09-02 22:48:27 +0000

    Update models

--- backend/minha_30287/wsgi.py
+++ backend/minha_30287/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'minha_30287.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "minha_30287.settings")
 
 application = get_wsgi_application()
